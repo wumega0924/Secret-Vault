ad69b60967516021ddaddfc068b95e49fd7a3d97
Steven <stevenwu4444@gmail.com>
2023-10-05 14:17:54 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// create an intro message for the user
+let message = "You have received this message because you have been chosen to open an importan vault. Here is the secret combination: ";
+// set up three variables with the combination of 10, 40, and 39 using arithmetic operators
+let firstNum = 15-5;
+let secondNum = 20 * 2;
+let thirdNum = 78 / 2;
+// combine the message var with the three numbers var
+let combinedMessage = message + firstNum + " , " + secondNum + " , " + thirdNum;
+// create a popup or dialog box to display the vault codes
+alert(combinedMessage);
